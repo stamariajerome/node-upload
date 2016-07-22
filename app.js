700dee897c82e56d470982c5cf0a92dc0f6e8933
Jerome Sta. Maria <stamaria.jerome@gmail.com>
2016-07-22 17:51:56 +0800

    added uuid generator

--- app.js
+++ app.js
@@ -1,49 +1,44 @@
 //* =========
 //  PACKAGES
 //* =========
-var express = require('express');
-var bodyParser = require('body-parser');
-var multer = require('multer');
+var express      = require('express'),
+    bodyParser   = require('body-parser'),
+    multer       = require('multer');
 
 //* ========
 //  HELPERS
 //* ========
-var helper = require('../helpers');
+var helper = require('./helpers');
 
-
-
+//* ==============
+//  CONFIGURATION
+//* ==============
+// custom file upload configruation
 var storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    console.dir(req);
-    console.dir(file);
-    console.dir(cb);
-    cb(null, '/public/uploads');
+    cb(null, 'public/uploads');
   },
   filename: function(req, file, cb) {
-    cb(null, file.originalname + helper.getFileExntesion(file.mimetype));
+    //TODO loops through all files and renaming them and modify helper
+    file = helper.uuid(file);
+    cb(null, file.originalname + helper.getFileExtension(file.mimetype));
   }
 });
-
-// file upload destination
-var upload = multer({dest: 'public/uploads/'});
-
+//if default storage is not avaiable use the custom storage.
+var dest = 'public/uploads/' || storage;
+var upload = multer({storage: storage});
 var app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 var files = [];
 
 app.get('/', function(req, res) {
-  console.log(files);
   res.render('index', {files: files});
 });
 
-//TODO come-up with a woraround renamign the  filename before storing
-// there are multiple types upload.single, upload.array, etc.
 app.post('/', upload.any(), function(req, res) {
   req.files.forEach(function(file){
-    file.filename = file.originalname;
     files.push(file);
    });
   res.redirect('/');
--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,24 @@
+uuid = require('node-uuid');
 var helper = {};
 
+helper.uuid = function(files) {
+  for(var x in files) {
+    files[x].originalname = uuid.v1(files[x].originalname);
+  }
+  return files;
+};
 
-helper.getFileExntesion = function(file) {
-  var lastDotArray = file.lastIndexOf('.');
-  extension = file.substr(lastDotArray);
+helper.convertFileExtension = function(extension) {
+  if(extension === 'jpeg') {
+    var convertedExtension = 'jpg';
+    return convertedExtension;
+  }
+};
+
+helper.getFileExtension = function(files) {
+  var lastSlashArray = files.lastIndexOf('/') + 1;
+
+  extension = '.' + helper.convertFileExtension(files.substr(lastSlashArray));
   return extension;
 };
 
